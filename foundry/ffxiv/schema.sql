baf928027eecb45b36689817d30c0c4b778394dd
azraelgnosis <azraelgnosis@gmail.com>
2020-03-29 00:45:15 -0400

    Adjust recipe schema

--- foundry/ffxiv/schema.sql
+++ foundry/ffxiv/schema.sql
@@ -1,5 +1,8 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS recipes;
+DROP TABLE IF EXISTS recipe_materials;
+DROP TABLE IF EXISTS components;
+DROP TABLE IF EXISTS map_recipe_component;
 DROP TABLE IF EXISTS locations;
 DROP TABLE IF EXISTS recipes;
 
@@ -10,33 +13,30 @@ CREATE TABLE users (
 );
 
 CREATE TABLE recipes (
-    recipe_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    pk INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
-    job TEXT NOT NULL,
     level INTEGER,
     type TEXT,
-    num_crafted INTEGER,
+    yield INTEGER,
     difficulty INTEGER,
     durability INTEGER,
-    max_quality INTEGER
+    max_quality INTEGER,
+    crystalA TEXT,
+    num_crystalA INTEGER,
+    crystalB Text,
+    num_crystalB INTEGER
 );
 
--- CREATE TABLE locations (
---     location_id INTEGER PRIMARY KEY AUTOINCREMENT,
---     within INTEGER,
---     name TEXT NOT NULL,
---     type TEXT NOT NULL,
---     FOREIGN KEY (within) REFERENCES locations (location_id)
--- );
+CREATE TABLE components (
+    pk INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
 
--- CREATE TABLE recipes (
---     recipe_id INTEGER PRIMARY KEY AUTOINCREMENT,
---     name TEXT NOT NULL,
---     job TEXT,
---     level INTEGER,
---     type TEXT,
---     num_crafted INTEGER,
---     difficulty INTEGER,
---     durability INTEGER,
---     max_quality INTEGER,
--- );
+CREATE TABLE map_recipe_component (
+    pk INTEGER PRIMARY KEY AUTOINCREMENT,
+    recipe_fk INTEGER NOT NULL,
+    component_fk INTEGER NOT NULL,
+    num INTEGER NOT NULL,
+    FOREIGN KEY (recipe_fk) REFERENCES recipes (pk),
+    FOREIGN KEY (component_fk) REFERENCES components (pk)
+);
