5c2f0185c3f3c05c1c81a24c7c81de888460a863
azraelgnosis <azraelgnosis@gmail.com>
2020-05-27 23:51:56 -0400

    Remove Democracy 3 Content

--- foundry/dem3/dem3.py
+++
@@ -1,9 +0,0 @@
-from flask import(
-    Blueprint, render_template
-)
-
-bp = Blueprint('dem3', __name__, url_prefix='/dem3')
-
-@bp.route('/')
-def index():
-    return render_template('dem3/index.html')
